3a866aa9a04ff7545841b5b50bc797511c51f883
DucooError <99142490+DucooError@users.noreply.github.com>
2022-06-05 00:53:18 +0300

    Update CooPhish.sh

--- CooPhish.sh
+++ CooPhish.sh
@@ -16,7 +16,7 @@ printf "\e[1;92m | |      | (   ) || |   | |\e[0m\e[1;77m| (      | (   ) |   | 
 printf "\e[1;92m | (____/\| (___) || (___) |\e[0m\e[1;77m| )      | )   ( |___) (___/\____) || )   ( |\e[0m\n"
 printf "\e[1;92m (_______/|/_____\||/_____\|\e[0m\e[1;77m|/       |/     \|\_______/\_______)|/     \|\e[0m\n"
 printf " \e[1;93m CooPhish Ver 1.0 \e[0m \n"
-printf " \e[1;77m powered by Duale | github.com/DucooError \e[0m \n"
+printf " \e[1;77m Powered by Duale | github.com/DucooError \e[0m \n"
 
 printf "\n"
 
